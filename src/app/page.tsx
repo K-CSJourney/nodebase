7817f4ae03d1421553a1cff391f5446016edd4ab
kbws13 <hsy040506@163.com>
2026-01-11 16:54:03 +0800

    ai-providers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,36 @@
-import { requireAuth } from "@/lib/auth-utils";
-import { caller } from "@/trpc/server";
+'use client';
+
+import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "sonner";
 import { LogoutButton } from "./logout";
 
-const Page = async () => {
-  await requireAuth();
+const Page = () => {
+  const trpc = useTRPC();
+  const create = useMutation(
+    trpc.createWorkflows.mutationOptions({
+      onSuccess: () => {
+        toast.success("Workflow created successfully");
+      },
+    })
+  );
 
-  const data = await caller.getUsers();
+  const testAi = useMutation(trpc.testAi.mutationOptions({
+    onSuccess: () => {
+      toast.success("AI job queued successfully");
+    }
+  }));
+
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
       protected server component
-      <div>{JSON.stringify(data, null, 2)}</div>
+      <Button disabled={testAi.isPending} onClick={() => testAi.mutate()}>
+        Test AI
+      </Button>
+      <Button disabled={create.isPending} onClick={() => create.mutate()}>
+        Create Wokflow
+      </Button>
       <LogoutButton />
     </div>
   );
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,6 +1,8 @@
-import prisma from "@/lib/db";
-import { baseProcedure, createTRPCRouter, protectedProcedure } from "../init";
 import { inngest } from "@/inngest/client";
+import prisma from "@/lib/db";
+import { google } from "@ai-sdk/google";
+import { generateText } from "ai";
+import { createTRPCRouter, protectedProcedure } from "../init";
 
 export const appRouter = createTRPCRouter({
   getUsers: protectedProcedure.query(({ ctx }) => {
@@ -15,12 +17,18 @@ export const appRouter = createTRPCRouter({
     await inngest.send({
       name: "test/hello-world",
       data: {
-        email: "kbws13@163.com"
-      }
-    })
+        email: "kbws13@163.com",
+      },
+    });
 
     return { success: true, message: "Workflow creation triggered." };
-  })
+  }),
+  testAi: protectedProcedure.mutation(async () => {
+    await inngest.send({
+      name: "execute/ai",
+    });
+    return { success: true, message: "Job queued" };
+  }),
 });
 
 export type AppRouter = typeof appRouter;
