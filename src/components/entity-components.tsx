295ab089d2ac82c6c652e6f50e2c073168f569e9
kbws13 <hsy040506@163.com>
2026-01-17 20:23:06 +0800

    editor

--- src/components/entity-components.tsx
+++ src/components/entity-components.tsx
@@ -183,7 +183,7 @@ export const LoadingView = ({ message }: StateViewProps) => {
 export const ErrorView = ({ message }: StateViewProps) => {
   return (
     <div className="flex justify-center items-center h-full flex-1 flex-col gap-y-4">
-      <AlertTriangleIcon className="size-6 animate-spin text-primary" />
+      <AlertTriangleIcon className="size-6 text-primary" />
       {!!message && <p className="text-sm text-muted-foreground">{message}</p>}
     </div>
   );
